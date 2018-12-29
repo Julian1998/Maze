3f0c62a03a68b8dbd4ba806650804fe789bd6486
Unknown <kimni.simon@gmail.com>
2018-12-29 18:20:50 +0100

    back to the future

---
+++ Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int rotationOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #if UNITY_ANDROID
+        
+        #else
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(rotationOffset,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Rotate(-rotationOffset,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(0,-rotationOffset,0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(0,rotationOffset,0);
+        }
+        #endif
+       
+    }
+}
---
+++ Assets/Options.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class Options : MonoBehaviour
+{
+    public Boolean shadow;
+    public DirectionalLight MAZELIGHT;
+    
+    public void ShadowSwitch()
+    {
+        if (shadow)
+        {
+           // MAZELIGHT = GetComponent(LightShadows.Soft);
+        }
+        
+        else
+        {
+           // MAZELIGHT = GetComponent(LightShadows.None);
+        }
+    }
+}
