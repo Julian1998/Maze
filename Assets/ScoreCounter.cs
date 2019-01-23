d1debd1162de8ae2f7b87b7e71138ffd63bd4fa7
Unknown <kimni.simon@gmail.com>
2019-01-23 15:58:56 +0100

    TRIGGER BABY

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+	
+        public Text scoreText;
+        public static int Scorer;
+
+
+        void Start(){
+            scoreText = GetComponent<Text> ();
+        }
+
+        // Update is called once per frame
+        void Update () {
+            scoreText.text = "Score: " + Scorer;
+        }
+    }
+
+
---
+++ Assets/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    void OnTriggerEnter (Collider G) {
+        if (G.tag == "Player") {
+            ScoreCounter.Scorer += 10;
+            Debug.Log (ScoreCounter.Scorer);
+            
+        }
+    }
+}
+
