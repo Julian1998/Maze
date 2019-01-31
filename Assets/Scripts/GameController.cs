410d5eb13dd2444584791239c6c69415f4eeda44
Julian Müller <julimueller1998@gmail.com>
2019-01-31 19:32:37 +0100

    I go vegan

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,26 +21,39 @@ public class GameController : MonoBehaviour
     void Update()
     {
         #if UNITY_ANDROID
-        transform.rotation = gyros.attitude;
+        Debug.Log(Input.acceleration);
+        //transform.rotation = Quaternion.Euler(-90 + gyros.gravity.x * 10, 90 + gyros.gravity.y * 10, 0);
+        transform.rotation *= Quaternion.Euler(0, 1, 0);
+        float minRotation = -45;
+        float maxRotation = -135;
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation, maxRotation);
+        transform.rotation = Quaternion.Euler (currentRotation);
+        //transform.Rotate(Input.acceleration.x, Input.acceleration.y, 0);
 
 #else
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.Rotate(rotationOffset,0,0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.Rotate(-rotationOffset,0,0);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.Rotate(0,-rotationOffset,0);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.Rotate(0,rotationOffset,0);
-        }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                transform.Rotate(rotationOffset,0,0);
+            }    
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.Rotate(-rotationOffset,0,0);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Rotate(0,-rotationOffset,0);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                transform.Rotate(0,rotationOffset,0);
+            }
 #endif
-
+    }
+    
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        //Debug.Log(q.eulerAngles);
+        return (new Quaternion(q.x, q.y, 0, 0) * Quaternion.Euler(-90, 90, 0));
     }
 }
