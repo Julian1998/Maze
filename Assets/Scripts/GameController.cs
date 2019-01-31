5c65c0304700ad148e7db7aaed8da2968059bcfe
Julian Müller <julimueller1998@gmail.com>
2019-01-31 20:17:15 +0100

    Acceleration is working

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public int rotationOffset;
+    private Rigidbody rb;
 
-    private Gyroscope gyros;
     // Start is called before the first frame update
     void Start()
     {
-        gyros = Input.gyro;
-        if(!gyros.enabled)
-        {
-            gyros.enabled = true;
-        }
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,12 +19,16 @@ public class GameController : MonoBehaviour
         #if UNITY_ANDROID
         Debug.Log(Input.acceleration);
         //transform.rotation = Quaternion.Euler(-90 + gyros.gravity.x * 10, 90 + gyros.gravity.y * 10, 0);
-        transform.rotation *= Quaternion.Euler(0, 1, 0);
+        //transform.rotation *= Quaternion.Euler(0, 1, 0);
         float minRotation = -45;
         float maxRotation = -135;
-        Vector3 currentRotation = transform.rotation.eulerAngles;
-        currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation, maxRotation);
-        transform.rotation = Quaternion.Euler (currentRotation);
+        Vector3 tilt = Input.acceleration;
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        rb.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.red);
+        //Vector3 currentRotation = transform.rotation.eulerAngles;
+        //currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation, maxRotation);
+        //transform.rotation = Quaternion.Euler (-90 + tilt * Mathf.Clamp(Input.acceleration.x, -1, 1) , 90 + tilt * Mathf.Clamp(Input.acceleration.y, -1, 1), 0);
         //transform.Rotate(Input.acceleration.x, Input.acceleration.y, 0);
 
 #else
