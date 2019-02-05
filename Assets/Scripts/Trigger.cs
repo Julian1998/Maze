fcd70dcd46ed6803595e7678e0b6e3a1ac207d3c
Julian Müller <julimueller1998@gmail.com>
2019-02-05 17:57:48 +0100

    Quality adjusting

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -7,13 +7,13 @@ public class Trigger : MonoBehaviour
 {
     public int pointValue;
 
-    
+
     void OnTriggerEnter (Collider G) {
         if (G.tag == "Player") {
             Debug.Log (pointValue);
             PlayerPrefs.SetInt("Score", pointValue);
             Application.LoadLevelAdditive(1);
+            Handheld.Vibrate();
         }
     }
 }
-
